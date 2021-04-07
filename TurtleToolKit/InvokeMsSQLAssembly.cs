da516fbc512bd9f762085135c1992e8a4ddc9bb7
unknown <calonso@attact.io>
2021-04-06 20:34:00 -0400

    sql todo

---
+++ TurtleToolKit/InvokeMsSQLAssembly.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurtleToolKit
+{
+    class InvokeMsSQLAssembly
+    {
+        // TO DO add assembly that passed ip and port gets netcat like reverse shell
+    }
+}
